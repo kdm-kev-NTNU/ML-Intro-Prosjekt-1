2810a08d42e342646ad95ebdda2d25bc5caba668
kdm-kev-NTNU <kevindm@stud.ntnu.no>
2025-08-27 08:52:41 +0200

    videre læring av diverse metoder

--- utforsking av metoder/numpy_utforskning.py
+++ utforsking av metoder/numpy_utforskning.py
@@ -1,8 +1,19 @@
 import numpy as np
 
+#np.mean eksempel
 y_true = np.array([0, 1, 1, 0, 1])   # true labels
 y_pred = np.array([0, 1, 0, 0, 1])   # predicted label
 
 accuracy = np.mean(y_true == y_pred)
 print(accuracy)  #betyr at det er 80% likhet mellom true labels og det som ble predicted
 
+
+
+#np.matmul
+
+#fungerer her som en dot product fordi det begge vektorene er i 1D. men det gjør kort sagt matrise multiplikasjon
+a = np.array([1, 2, 3])
+b = np.array([4, 5, 6])
+
+result = np.matmul(a, b)
+print(result)  # 32 -- 1*4 + 2*5 + 3*6 = 32
