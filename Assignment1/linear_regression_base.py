b6451a0b5eee8f6fea2aebfcf52b3fe635e6780b
kdm-kev-NTNU <kevindm@stud.ntnu.no>
2025-08-27 09:48:41 +0200

    restructuring of accuracy method

--- Assignment1/linear_regression_base.py
+++ Assignment1/linear_regression_base.py
@@ -23,7 +23,7 @@ class LinearRegression():
     def update_parameters(self, grad_w, grad_b):
         pass
 
-    def accuracy(true_values, predictions):
+    def accuracy(self, true_values, predictions):
         return np.mean(true_values == predictions)
 
         """
@@ -35,24 +35,20 @@ class LinearRegression():
             y (array<m>): a vector of floats
         """
     def fit(self, X, y):
-        self.weights = np.zeros(x.shape[1]) # x.shape = antall datapunkter, antall features
+        self.weights = np.zeros(X.shape[1]) # X.shape = antall datapunkter, antall features
         self.bias = 0
         # Gradient Descent
         for _ in range(self.epochs): #Epochs: antall ganger treningsdataene sendes gjennom læringsalgoritmen. Nok en hyperparameter
-            lin_model = np.matmul(self.weights, x.transpose()) + self.bias # her brukes det tranpose slik at matrimultiplikasjonen skal være på riktig form (m x n) * (n x p)
+            lin_model = np.matmul(self.weights, X.transpose()) + self.bias # her brukes det tranpose slik at matrimultiplikasjonen skal være på riktig form (m x n) * (n x p)
             y_pred = self.sigmoid_function(lin_model) #her begrenses de verdiene som lin modellen kan lage, mellom [0, 1] slik at det 
             grad_w, grad_b = self.compute_gradients(x, y, y_pred)
             self.update_parameters(grad_w, grad_b)
 
             loss = self._compute_loss(y, y_pred)  #her regnes ut loss - f.eks. via cross-entropi loss
             pred_to_class = [1 if _y > 0.5 else 0 for _y in y_pred] #her er klassifiseringsterskelen 0.5 er den større en det blir det 1 ellers 0. 
-            self.train_accuracies.append(accuracy(y, pred_to_class))
+            self.train_accuracies.append(self.accuracy(y, pred_to_class))
             self.losses.append(loss)
 
-
-
-        # TODO: Implement
-        raise NotImplementedError("The fit method is not implemented yet.")
 
         """
         Generates predictions
@@ -67,7 +63,9 @@ class LinearRegression():
             A length m array of floats
         """ 
     def predict(self, X):
-        lin_model = np.matmul(x, self.weights)
+        lin_model = np.matmul(x, self.weights) + self.bias
+        y_pred = self.sigmoid_function(lin_model)
+        return [1 if _y > 0.5 else 0 for _y in y_pred]
 
         
 
