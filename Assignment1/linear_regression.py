d6b9fe99b7ecde6aafa513b9aca98466c6198687
kdm-kev-NTNU <kevindm@stud.ntnu.no>
2025-08-27 08:39:41 +0200

    initialisering av Lin Reg metoder for Fit og Predict

--- Assignment1/linear_regression.py
+++ Assignment1/linear_regression.py
@@ -2,10 +2,30 @@ import numpy as np
 
 class LinearRegression():
     
-    def __init__(self):
+    def __init__(self, learning_rate=0.1, epochs=100):
         # NOTE: Feel free to add any hyperparameters 
         # (with defaults) as you see fit
+        self.learning_rate = learning_rate
+        self.epochs = epochs
+        self.weights, self.bias = None, None #Dette er ikke en tuple, det er bare en mer lettvint måte å skrive attributter på
+        self.losses, self.train_accuracies = [], []
+
+
+    def sigmoid_function(self, x):
         pass
+
+    def _compute_loss(self, y, y_pred):
+        pass
+
+    def compute_gradients(self, x, y, y_pred):
+        pass
+
+    def update_parameters(self, grad_w, grad_b):
+        pass
+
+    def accuracy(true_values, predictions):
+        return np.mean(true_values == predictions)
+
         
     def fit(self, X, y):
         """
