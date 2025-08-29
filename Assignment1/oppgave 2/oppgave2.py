220e7149986f50d86888a27bb47f57709da8c638
kdm-kev-NTNU <kevindm@stud.ntnu.no>
2025-08-29 16:41:04 +0200

    oppgave 2 arbeid import av chapter 1

--- Assignment1/oppgave 2/oppgave2.py
+++ Assignment1/oppgave 2/oppgave2.py
@@ -2,7 +2,7 @@ import numpy as np
 
 class LinearRegression():
     
-    def __init__(self, learning_rate=0.1, epochs=100):
+    def __init__(self, learning_rate=0.1, epochs=1000):
         self.learning_rate = learning_rate
         self.epochs = epochs
         self.weights, self.bias = None, None 
